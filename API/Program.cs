88e9b2d7a9c0f5e3b37876dfb310b5b8ab1968ad
trinhtam <trinhtam2001@gmail.com>
2024-05-28 14:28:15 +0700

    allow CORs

--- API/Program.cs
+++ API/Program.cs
@@ -67,7 +67,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(p=>p.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
