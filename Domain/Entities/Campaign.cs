4f99ccc1d015f3f2c9d7f373f942d55a3be5d77c
trinhtam <trinhtam2001@gmail.com>
2024-06-01 18:39:26 +0700

    addImage

--- Domain/Entities/Campaign.cs
+++ Domain/Entities/Campaign.cs
@@ -10,9 +10,10 @@ namespace Domain.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string ScopeOfWork { get; set; }
-        public string Requirements { get; set; }
+        public string? ScopeOfWork { get; set; }
+        public string? Requirements { get; set; }
         public int Duration { get; set; }
+        public string? ImagePath { get; set; }
 
         public List<CampaignTrainingProgram> CampaignTrainingPrograms { get; set; }
 
--- Domain/Entities/TrainingProgram.cs
+++ Domain/Entities/TrainingProgram.cs
@@ -10,12 +10,13 @@ namespace Domain.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string ScopeOfWork { get; set; }
-        public string Requirements { get; set; }
-        public string Benefits { get; set; }
-        public DateTime StartDate { get; set; }
+        public string? ScopeOfWork { get; set; }
+        public string? Requirements { get; set; }
+        public string? Benefits { get; set; }
+        public DateTime? StartDate { get; set; }
         public int Duration { get; set; }
         public int TotalMember { get; set; }
+        public string? ImagePath { get; set; }
 
         public List<CampaignTrainingProgram> CampaignTrainingPrograms { get; set; }
         public List<UserAccount> Interns { get; set; }
