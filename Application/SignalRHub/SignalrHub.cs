8e47d709341ae2fe438a52578fa938eaa02854fe
trinhtam <trinhtam2001@gmail.com>
2024-06-13 08:06:23 +0700

    signalR

--- Application/SignalRHub/SignalrHub.cs
+++ Application/SignalRHub/SignalrHub.cs
@@ -1,8 +1,20 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Application.SignalRHub.Model;
+using Microsoft.AspNetCore.SignalR;
 public class SignalrHub : Hub
 {
     public async Task NewMessage(string user, string message)
     {
         await Clients.All.SendAsync("messageReceived", user, message);
     }
+
+
+    public async Task JoinRoom(UserConnection userConnection)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
+        await Clients.All.SendAsync("ReceiveMessage", "_botUser",
+        $"{userConnection.User} has joined {userConnection.Room}");
+
+
+    }
+
 }
