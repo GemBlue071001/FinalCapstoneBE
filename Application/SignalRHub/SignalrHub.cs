4e969ee2f07c0e1012f21b681c72f5c6536df7eb
trinhtam <trinhtam2001@gmail.com>
2024-06-18 07:43:00 +0700

    fix signalR

--- Application/SignalRHub/SignalrHub.cs
+++ Application/SignalRHub/SignalrHub.cs
@@ -1,20 +1,40 @@
 ﻿using Application.SignalRHub.Model;
 using Microsoft.AspNetCore.SignalR;
+
 public class SignalrHub : Hub
 {
-    public async Task NewMessage(string user, string message)
+    private readonly string _botUser;
+    private readonly IDictionary<string, UserConnection> _connections;
+
+    public SignalrHub(IDictionary<string, UserConnection> connections)
     {
-        await Clients.All.SendAsync("messageReceived", user, message);
+        _botUser = "MyChat Bot";
+        _connections = connections;
     }
+
 
 
     public async Task JoinRoom(UserConnection userConnection)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, userConnection.Room);
-        await Clients.All.SendAsync("ReceiveMessage", "_botUser",
-        $"{userConnection.User} has joined {userConnection.Room}");
 
+        _connections[Context.ConnectionId] = userConnection;
+
+        await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", _botUser, $"{userConnection.User} has joined {userConnection.Room}");
 
     }
 
+
+    public async Task SendMessage(string message)
+    {
+        if (_connections.TryGetValue(Context.ConnectionId, out UserConnection userConnection))
+        {
+            await Clients.Group(userConnection.Room).SendAsync("ReceiveMessage", userConnection.User, message);
+        }
+    }
+
+    public async Task NewMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("messageReceived", user, message);
+    }
 }
