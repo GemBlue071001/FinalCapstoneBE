312969ed7eac4398ef932953973224d235bf86e9
Hiep <hoanghiep8401@gmail.com>
2024-10-11 22:38:05 +0700

    up

--- Application/Services/FollowCompanyService.cs
+++ Application/Services/FollowCompanyService.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Application.Interface;
+using Application.Request.FollowCompany;
+using Application.Response;
+using Application.Response.FollowCompany;
+using AutoMapper;
+using Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,54 @@ using System.Threading.Tasks;
 
 namespace Application.Services
 {
-    internal class FollowCompanyService
+    public class FollowCompanyService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        private readonly IMapper _mapper;
+        private IClaimService _claimService;
+        public FollowCompanyService(IUnitOfWork unitOfWork, IMapper mapper, IClaimService claimService)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _claimService = claimService;
+        }
+        public async Task<ApiResponse> AddFollowCompanyAsync(FollowCompanyRequest followCompanyRequest)
+        {
+            ApiResponse apiResponse = new ApiResponse();
+            try
+            {
+                var claim = _claimService.GetUserClaim();
+                var company = await _unitOfWork.Companys.GetAsync(x => x.Id == followCompanyRequest.CompanyId);
+                if (company != null)
+                {
+                    return apiResponse.SetBadRequest("Not found Company " + followCompanyRequest.CompanyId);
+                }
+                var followCompany = _mapper.Map<FollowCompany>(followCompanyRequest);
+                followCompany.UserId = claim.Id;
+                await _unitOfWork.FollowCompanies.AddAsync(followCompany);
+                await _unitOfWork.SaveChangeAsync();
+                return apiResponse.SetOk("Add Success");
+
+            }
+            catch (Exception ex)
+            {
+                return apiResponse.SetBadRequest(ex.Message);
+            }
+        }
+        public async Task<ApiResponse> getFollowCompanyAsync() 
+        {
+            ApiResponse apiResponse = new ApiResponse();
+            try
+            {
+                var claim = _claimService.GetUserClaim();
+                var followCompanies = await _unitOfWork.FollowCompanies.GetAllAsync(x => x.UserId == claim.Id);
+                var responseList = _mapper.Map<List<FollowCompanyResponse>>(followCompanies)l
+            }
+            catch (Exception ex)
+            {
+                return apiResponse.SetBadRequest(ex.Message);
+            }
+        }
     }
 }
