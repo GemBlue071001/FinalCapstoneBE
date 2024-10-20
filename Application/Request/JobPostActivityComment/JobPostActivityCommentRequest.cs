7dbdae4be42a23ae34adcbe9907468d62c38bffc
MSI\PC <lilo190516@gmail.com>
2024-10-20 19:22:25 +0700

    reefactor comment, add paging

--- Application/Request/JobPostActivityComment/JobPostActivityCommentRequest.cs
+++ Application/Request/JobPostActivityComment/JobPostActivityCommentRequest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.Request.JobPostActivityComment
+﻿namespace Application.Request.JobPostActivityComment
 {
     public class JobPostActivityCommentRequest
     {
