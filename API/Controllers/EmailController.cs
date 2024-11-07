3700a73129365eba3a4d41fdf6b0079c81082034
Hiep <hoanghiep8401@gmail.com>
2024-11-07 22:53:38 +0700

    custom email

---
+++ API/Controllers/EmailController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interface;
+using Application.Request.EducationDetail;
+using Application.Request.Email;
+using Application.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private IEmailService _service;
+
+        public EmailController(IEmailService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CustomSendEmail(CustomSendEmailRequest request)
+        {
+            var response = await _service.CustomSendEmail(request.ReciveUser,request.Content , request.CompanyName);
+            return response.IsSuccess ? Ok(response) : BadRequest(response);
+        }
+    }
+}
--- Application/Interface/IEmailService.cs
+++ Application/Interface/IEmailService.cs
@@ -6,5 +6,6 @@ namespace Application.Interface
     {
         Task<ApiResponse> SendMail(string recievedUser, string emailContent, string userName, string company, string jobTitle);
         Task<ApiResponse> SendValidationEmail(string recievedUser, string emailContent);
+        Task<ApiResponse> CustomSendEmail(string recievedUser, string emailContent, string companyName);
     }
 }
