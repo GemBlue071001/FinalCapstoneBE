c8d819adbda0f68ce24ca3a23c6b975a566cb2fa
trinhtam <trinhtam2001@gmail.com>
2024-11-12 01:10:46 +0700

    uptime

--- Application/Services/FileHandlingService.cs
+++ Application/Services/FileHandlingService.cs
@@ -110,7 +110,7 @@ namespace Application.Services
             //    return new ApiResponse().SetBadRequest("File must be a PDF");
             //}
 
-            using (var httpClient = new HttpClient() { Timeout = TimeSpan.FromSeconds(180) })
+            using (var httpClient = new HttpClient() { Timeout = TimeSpan.FromSeconds(300) })
             {
                 //var requestContent = new MultipartFormDataContent();
                 //var fileContent = new StreamContent(file.OpenReadStream());
