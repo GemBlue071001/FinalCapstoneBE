749dfc9c64baaea44a47f2c6fef3f9cf4b9c8cbf
trinhtam <trinhtam2001@gmail.com>
2024-11-18 20:42:50 +0700

    update controller

--- API/Controllers/FollowJobPostController.cs
+++ API/Controllers/FollowJobPostController.cs
@@ -2,6 +2,7 @@
 using Application.Request.FollowCompany;
 using Application.Request.FollowJob;
 using Application.Request.JobLocation;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +17,8 @@ namespace API.Controllers
         {
             _service = service;
         }
+
+        [Authorize]
         [HttpPost]
         public async Task<IActionResult> AddFollowJobPostAsync(FollowJobRequest followJobRequest)
         {
@@ -23,12 +26,15 @@ namespace API.Controllers
             return response.IsSuccess ? Ok(response) : BadRequest(response);
         }
 
+        [Authorize]
         [HttpGet]
         public async Task<IActionResult> GetFollowJobPostAsync()
         {
             var response = await _service.GetFollowJobPostAsync();
             return response.IsSuccess ? Ok(response) : BadRequest(response);
         }
+
+        [Authorize]
         [HttpDelete]
         public async Task<IActionResult> DeletedSeekerProfileByIdAsync(int id)
         {
