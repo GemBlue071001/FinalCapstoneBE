5a5c6ba44a30a323bce332ac2c3aed68f4f425b6
LAPTOP-E0FIB2AJ\trinh <trinhtam2001@gmail.com>
2024-11-25 10:51:53 +0700

    update FE link

--- API/Controllers/PaymentController.cs
+++ API/Controllers/PaymentController.cs
@@ -37,12 +37,12 @@ namespace API.Controllers
             if (response.IsSuccess)
             {
                 // Extract the redirect URL from the response and pass it as a query parameter to the FE
-                var redirectUrl = "http://localhost:5173/it-jobs?status=success";
+                var redirectUrl = "https://jobsearch-zeta-nine.vercel.app/it-jobs?status=success";
                 return Redirect(redirectUrl);
             }
             else
             {
-                var redirectUrl = "http://localhost:5173/it-jobs?status=failure";
+                var redirectUrl = "https://jobsearch-zeta-nine.vercel.app/it-jobs?status=failure";
                 return Redirect(redirectUrl);
             }
         }
